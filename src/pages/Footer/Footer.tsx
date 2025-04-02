b77ee85421cb591c26b9d5d4ff070e04928fc6e9
Christian Armenta <car_arca@hotmail.com>
2025-04-01 21:14:43 -0700

    feat: added footer

--- src/pages/Footer/Footer.tsx
+++ src/pages/Footer/Footer.tsx
@@ -1,7 +1,19 @@
+import style from "./Footer.module.css";
+
 export const Footer = () => {
-    return (
-        <div>
-            Footer
+  return (
+    <>
+      <div className={style.footer}>
+        <p className={style.copyright}>
+          © 2025 Shield Plus. All Rights Reserved.
+        </p>
+
+        <div className={style.links}>
+          <a href="">Terms & Conditions</a>
+          <a href="">Privacy Policy</a>
+          <a href="">Licensing</a>
         </div>
-    )
-}
+      </div>
+    </>
+  );
+};
